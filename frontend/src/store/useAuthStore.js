7c1780c50d34dc75901fbd68e67c02facbb35fdb
muhammadadilbankar <venusmars2004@gmail.com>
2025-04-11 08:51:46 +0530

    trying to deploy

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
-import { get } from "mongoose";
+// import { get } from "mongoose";
 import { io } from "socket.io-client";
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
